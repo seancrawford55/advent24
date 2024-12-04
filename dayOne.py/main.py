2621b42cb21867918ea641c7cb435a7f9e4f016d
Sean Crawford <Seancrawford55@gmail.com>
2024-12-04 10:50:37 -0600

    Functioning solution to part two

--- dayOne.py/main.py
+++ dayOne.py/main.py
@@ -18,10 +18,22 @@ def subtraction(lis1, lis2):
         total += (math.fabs(lis1[i] - lis2[i]))
     return total
 
+def similarityScore(leftList, rightList):
+    score = 0
+    counter = 0
+    for i in range(len(leftList)):
+        for n in range(len(rightList)):
+            if (rightList[n] == leftList[i]):
+                counter += 1
+        score += (leftList[i] * counter)
+        counter = 0
+    return score
 
 list1, list2 = read_columns('ex.txt')
 
 runningTotal = subtraction(list1, list2)
 
+simScore = similarityScore(list1, list2)
 
+print(simScore)
 print(runningTotal)
